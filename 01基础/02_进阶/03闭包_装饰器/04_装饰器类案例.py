77cc7c432f4f1c8eb49d8c70d2c6cee8fc96d067
XF <xiafan98@163.com>
2026-03-31 22:42:06 +0800

    装饰器类案例

--- 01基础/02_进阶/03闭包_装饰器/04_装饰器类案例.py
+++ 01基础/02_进阶/03闭包_装饰器/04_装饰器类案例.py
@@ -5,10 +5,11 @@ from functools import wraps
 
 
 class LoginDoor(object):
-    def __init__(self, func):
-        self.func = func
+    def __init__(self, file):
+        self.file = file
         self.login_status = False
         self.users = []
+        self.func = None
 
         self.check_file()
 
@@ -33,72 +34,54 @@ class LoginDoor(object):
                 time.sleep(2)
                 sys.exit(1)
 
-    def __call__(self, *args, **kwargs):
-        if not self.login_status:
-            input_login_user = input('请输入登录账号')
-            input_login_pass = input('请输入登录密码')
-            for user in self.users:
-                if user['username'] == input_login_user and user['password'] == input_login_pass:
-                    self.login_status = True
-                    self.func()
-        else:
+    def __call__(self, func):
+        self.func = func
 
+        @wraps(func)  # 保留原函数的元数据
+        def wrapper(*args, **kwargs):
+            # 未登录时进行登录验证
+            if not self.login_status:
+                print('请先登录...')
+                input_login_user = input('请输入登录账号: ')
+                input_login_pass = input('请输入登录密码: ')
 
-def login_door(file):
-    def login(func):
-        login_status = False
-        users = []
-        # 检查文件是否存在
-        is_exists = os.path.exists(file)
-        if is_exists:
-            with open(file) as read_stream:
-                lions = read_stream.readlines()
-                for lion in lions:
-                    user = eval(lion)
-                    users.append(user)
-        else:
-            choice = input('是否需要创建新用户：Y/N')
-            if choice == 'Y' or choice == 'y':
-                with open(file, mode='w') as write_stream:
-                    username = input('请输入用户名：')
-                    password = input("请输入密码：")
-                    user = {'username': username, 'password': password}
-                    users.append(user)
-                    write_stream.write(str(user))
-            else:
-                print('当前用户选择退出')
-                sys.exit()
-
-        # 使用 wraps 伪装调用时让外面知道还有内置的方法
-        @wraps(func)
-        def inner(*args, **kwargs):
-            nonlocal login_status
-            if not login_status:
-                input_user = input('登录用户名：')
-                input_pass = input('登录用户名：')
-                for un in users:
-                    if un['username'] == input_user and un['password'] == input_pass:
-                        login_status = True
-                        print('登录成功')
+                # 验证用户
+                login_success = False
+                for user in self.users:
+                    if (user['username'] == input_login_user and
+                            user['password'] == input_login_pass):
+                        login_success = True
                         break
-                    else:
-                        print('输入用户名密码有误')
-            if login_status:
-                return func(*args, **kwargs)
-            return None
 
-        return inner
+                if login_success:
+                    self.login_status = True
+                    print('登录成功！')
+                else:
+                    print('用户名或密码错误，无法执行操作')
+                    return None
 
-    return login
+            # 已登录，执行原函数
+            return self.func(*args, **kwargs)
 
+        return wrapper
 
-@login_door('store_file')
+@LoginDoor('store_file')
 def need_check_method():
+    """需要登录验证的方法"""
     print('执行查询方法')
 
+@LoginDoor('store_file')
+def add_data(data):
+    """带参数的需要登录验证的方法"""
+    print(f'添加数据: {data}')
 
 if __name__ == '__main__':
-    # 实际先调用装饰器函数中inner方法
+    print("=== 第一次调用 ===")
     need_check_method()
+
+    print("\n=== 等待2秒后第二次调用 ===")
     time.sleep(2)
     need_check_method()
+
+    print("\n=== 测试带参数的方法 ===")
+    add_data("测试数据")
