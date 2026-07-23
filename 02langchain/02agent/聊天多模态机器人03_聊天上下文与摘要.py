47075a3ff878ab7760ff606e113c2bafbbca7318
XF <xiafan98@163.com>
2026-07-23 15:26:49 +0800

    agent摘要

---
+++ 02langchain/02agent/聊天多模态机器人03_聊天上下文与摘要.py
@@ -0,0 +1,100 @@
+from langchain_core.chat_history import InMemoryChatMessageHistory
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain_core.runnables import RunnableWithMessageHistory
+from langchain_community.chat_message_histories import SQLChatMessageHistory
+from pyarrow.lib import null
+
+from my_llm import qwen37
+
+#  提示词模板
+prompt = ChatPromptTemplate.from_messages([
+    ('system', '你是个乐于助人助手 ，尽力回答你知道的问题，提供的来哦天里是包含 与你对话 用户的相关信息'),
+    MessagesPlaceholder(variable_name="chat_history", optional=True),
+    ('human', '{input}')
+])
+
+chain = prompt | qwen37
+
+# 存储聊天记录（内存 / 关系型数据库）
+
+# 使用字典格式保存不同用户的聊天记录
+store = {}
+
+
+# 定义工厂func，告诉大模型怎么存储与怎么返回历史记录
+def get_session_history(session_id: str):
+    """
+    从内存存储的历史消息列表中，返回当前会话的所有历史消息
+    """
+    return SQLChatMessageHistory(
+        session_id=session_id,
+        connection='sqlite:///chat_histories.db'
+    )
+
+
+# 历史消息种类：SystemMessage，HumanMessage，ToolMessage，AIMessage
+
+# 处理历史聊天记录
+chain_with_message_history = RunnableWithMessageHistory(
+    chain,
+    get_session_history,
+    input_messages_key='input',  # 用户输入消息key
+    history_messages_key='chat_history'  # 与提示词模板中定义的内容需要一致（老版本需要保持一致，新版本无所谓）
+)
+
+
+# 剪辑摘要上下文 ，历史记录：保留最近n条数据
+def summarize_messages(current_input: str):
+    """
+    剪辑摘要上下文
+    """
+    session_id = current_input['config']['configurable']['session_id']
+    if not session_id:
+        raise ValueError('需通过config参数提供session_id')
+
+    # 获取当前对话的历史消息
+    chat_history = get_session_history(session_id)
+    stored_messages = chat_history.messages
+
+    if len(stored_messages) <= 2:
+        return False
+
+    # 超过的数据进行摘要,保留最后两条消息，之前的消息生成摘要
+    last_message_history = stored_messages[-2:]
+    messages_to_summarize = stored_messages[:-2]
+
+    summarization_prompt = ChatPromptTemplate.from_messages(
+        [
+            ('system', '将一下对话历史压缩为一条保留关键信息的摘要信息'),
+            ('placeholder', '{chat_history}'),
+            ('human', '请生成包含上述对话核心内容的摘要，保留重要事实和决策')
+        ]
+    )
+
+    summarization_chain = summarization_prompt | qwen37
+    # 生成摘要
+    summary_message = summarization_chain.invoke({'chat_history': messages_to_summarize})
+
+    # 重构历史聊天记录
+    chat_history.clear()
+    chat_history.add_message(summary_message)
+    for msg in last_message_history:
+        chat_history.add_message(msg)
+
+    return True
+
+
+result = chain_with_message_history.invoke(
+    {
+        'input': '你好 我是夏帆'
+    }, {"configurable": {'session_id': 'user_id_123'}}
+)
+
+print(result)
+
+result1 = chain_with_message_history.invoke(
+    {
+        'input': '你好 我的名字是什么'
+    }, config={"configurable": {'session_id': 'user_id_123'}}
+)
+print(result1)
