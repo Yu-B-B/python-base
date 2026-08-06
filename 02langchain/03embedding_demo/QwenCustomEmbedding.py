c55677923372a2ee27344dda81ce1cc2319f1826
XF <xiafan98@163.com>
2026-08-06 20:07:05 +0800

    自定义embedding模型

--- 02langchain/03embedding_demo/QwenCustomEmbedding.py
+++ 02langchain/03embedding_demo/QwenCustomEmbedding.py
@@ -12,7 +12,7 @@ class QwenCustomEmbedding(Embeddings):
 
     # 对文本内容做向量化
     def embed_documents(self, texts: list[str]) -> list[list[float]]:
-        return self.embed_documents(texts)
+        return self.qwen3_embedding.encode(texts)
 
     # 对用户输入内容做向量化
     def embed_query(self, text: str) -> list[float]:
